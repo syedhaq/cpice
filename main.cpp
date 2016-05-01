efb9a7b57ff09827ba717bd66c8a92fabca2092f
Kaiyuan Liu <kevinliu24@gmail.com>
2016-05-01 01:29:48 -0400

    Fixed bug in RK34 Method

--- main.cpp
+++ main.cpp
@@ -5,18 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-    /*std::vector<double>init;
+    std::vector<double>init;
     init.push_back(2);
     spiceob A(1,1,5,init);
     
+    /*
     std::vector< std::vector<double> > finalv;
     finalv=A.fwdEuler();
 
     for(int i=0;i<5;i++){
         cout<<(finalv[0][i])<<endl;
-    } */
+    }
+     */
 
-    std::vector<double>init;
+    /*std::vector<double>init;
     init.push_back(0);
     init.push_back(0);
     spiceob A(2,1*pow(10,-9),100,init);
@@ -28,7 +30,7 @@ int main(int argc, char *argv[])
     for(int i=0;i<100;i++){
         cout<<"step "<<i<<" value: "<< finalv[0][i] <<endl;
     }
-    
+    */
     cout<<"\nRK34 Method Without Time adaptation\n";
     vector< vector<double> >test=A.rk34Nt();
     
