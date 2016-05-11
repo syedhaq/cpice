2fdf674eff12819f8ac37e20096e562010aab7c6
Kaiyuan Liu <kevinliu24@gmail.com>
2016-05-11 15:41:15 -0400

    Main Method

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
     
     
     //task 3:
+    /*
     cout<<"Task 4:\n";
     cout<<"Simulation for 1ns step size with 100 steps\n";
     A=*new spiceob(2,1*pow(10,-9),100,init);
@@ -32,7 +33,6 @@ int main(int argc, char *argv[]){
     
     writeModel("backwardEulerTask3_1ns.txt",finalv,"hor");
     
-    /*
     finalv=A.trapEuler();
     
     cout<<"\nTrapazoid Euler Method\n";
@@ -40,8 +40,12 @@ int main(int argc, char *argv[]){
         cout << "step "<<i<<" value: "<< finalv[1][i] <<endl;
     }
 
+    writeModel("trapazoidEulerTask4_1ns.txt",finalv,"hor");
+    */
+     
+     
+    cout<<"\nRK34 Method Without Time adaptation\n";
     vector< vector<double> >test=A.rk34Nt();
-
 
 
     A=*new spiceob(2,1*pow(10,-6),40*pow(10,-6),init);
@@ -55,35 +59,41 @@ int main(int argc, char *argv[]){
         //cout<<"Time:"<<test[i][2]<<test[i][1]<<endl;
 
     }
-    
-    writeModel("forwardEulerTask4_0.2ns.txt",finalv,"hor");
-    
-    finalv=A.trapEuler();
-    
-    cout<<"\nTrapazoid Euler Method\n";
-    for(int i=0;i<100;i++){
-        cout << "step "<<i<<" value: "<< finalv[1][i] <<endl;    }
-  
-    writeModel("trapazoidEulerTask4_0.2ns.txt",finalv,"hor");
-    
-    cout<<"\nRK34 Method Without Time adaptation\n";
-    test=A.rk34Nt();
-    
-    for(int i=0;i<test.size();i++)
-        cout << "step "<<i<<" value: "<< test[i][0] << endl;
-    
-    writeModel("RK34NTTask4_0.2ns.txt",test,"vert");
-    
-    A=*new spiceob(2,0.2*pow(10,-9),100*pow(10,-9),init);
-    
-    cout<<"\nRK34 Method With Time adaptation\n";
-    test=A.rk34T(pow(10,-1.0),pow(10,-6.0));
-    
-    for(int i=0;i<test.size();i++)
-        cout << "step "<<i<<" value: "<< test[i][0] << endl;
-    
-    writeModel("RK34TTask4_0.2ns.txt",test,"vert");
-    */
+
+    //Find v2 after 20us and initiate parameter search
+    bool found=false;
+    int index=0;
+    double dh=.000000005;
+    double par[]={1,pow(10,-8),6*pow(10,-8)};
+    vector<double>x1;
+    vector<double>x2;
+    vector<double>y;
+    for( index=0;index<test.size()&&found==false;index++){
+        if(test[index][2]>=(20*pow(10,-6))){
+                found=true;
+                x2.push_back(test[index][1]);
+
+
+
+        }
+
+    }
+
+
+    //Read all values after 20us for parameter optimization
+    cout<<test.size()<<endl;
+    for(int i=index;i<test.size();i=i+50){
+        //Push time
+        x1.push_back(test[i][2]);
+
+        //Push voltage
+        y.push_back(test[i][1]);
+    }
+
+     cout<<"Size of x1:"<<x1.size()<<endl;
+     cout<<x2[0];
+
+newton3(par,dh,x1,x2,y,x1.size());
     return 0;
 }
 
